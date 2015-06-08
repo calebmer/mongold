b69f3f877eb8fcc8d40c09eb4ad647ab907c5e14
Caleb Meredith <calebmeredith8@gmail.com>
2015-06-07 22:05:43 -0400

    Renames things to Mongold

--- test/database.js
+++ test/database.js
@@ -1,19 +1,19 @@
-var Humongo = require('../lib');
-var url = 'mongodb://localhost:27017/humongo-test';
+var Mongold = require('../lib');
+var url = 'mongodb://localhost:27017/mongold-test';
 
 describe('Database', function () {
 
   it('can connect and disconnect', function (done) {
 
-    var database = new Humongo.Database(url);
+    var database = new Mongold.Database(url);
     database.on('close', done);
     database.disconnect();
   });
 
   it('can connect and disconnect using the shortcut', function (done) {
 
-    Humongo.connect(url);
-    Humongo.database.on('close', done);
-    Humongo.disconnect();
+    Mongold.connect(url);
+    Mongold.database.on('close', done);
+    Mongold.disconnect();
   });
 });
--- test/model.js
+++ test/model.js
@@ -1,8 +1,8 @@
 var Assert = require('assert');
 var Async = require('async');
 var MongoClient = require('mongodb').MongoClient;
-var Humongo = require('../lib');
-var url = 'mongodb://localhost:27017/humongo-test';
+var Mongold = require('../lib');
+var url = 'mongodb://localhost:27017/mongold-test';
 
 describe('Model', function () {
 
@@ -24,26 +24,27 @@ describe('Model', function () {
 
   before(function (done) {
 
-    chocolate.database = new Humongo.Database(url);
-    chocolate.Misc = new Humongo.Model(collectionName, chocolate.database);
+    chocolate.database = new Mongold.Database(url);
+    chocolate.Misc = new Mongold.Model(collectionName, chocolate.database);
     chocolate.database.on('ready', function () { done(); });
   });
 
   after(function () { vanilla.db.close(); });
   after(function () { chocolate.database.disconnect(); });
 
+
   it('requires a database', function () {
     /* eslint-disable */
-    Assert.throws(function () { new Humongo.Model('model', undefined); }, /require(.*)database/i);
-    new Humongo.Model('model', chocolate.database);
+    Assert.throws(function () { new Mongold.Model('model', undefined); }, /require(.*)database/i);
+    new Mongold.Model('model', chocolate.database);
     /* eslint-enable */
   });
 
   it('can use the default database', function () {
     /* eslint-disable */
-    Humongo.connect(url);
-    new Humongo.Model('model');
-    Humongo.disconnect(url);
+    Mongold.connect(url);
+    new Mongold.Model('model');
+    Mongold.disconnect(url);
     /* eslint-enable */
   });
 
